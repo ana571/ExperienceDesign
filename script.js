66a9dbe75cf73c10bf72362f13f9330ba6637b35
ana571 <ana.tettoni@gmail.com>
2024-01-26 18:31:58 +0100

    wip

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const workshops = {
     3: ["portfolio/W3/froggy.png", "portfolio/W3/frogs.png", "portfolio/W3/noise.png", "portfolio/W3/solution.png", "portfolio/W3/sol_ill.png"],
     4: ["portfolio/W4/patent.png", "portfolio/W4/active.png", "portfolio/W4/acti.png", "portfolio/W4/inroom.png"],
     5: ["portfolio/W5/karenine.png", "portfolio/W5/bench.png", "portfolio/W5/pho.png", "portfolio/W5/restbench.png"],
-    6: ["portfolio/W6/galicienne.png", "portfolio/W6/chat.png", "portfolio/W6/collage.png", "portfolio/W6/galicienne.png", "portfolio/W6/inspo.png", "portfolio/W6/image.png"]
+    6: ["portfolio/W6/galicienne.jpg", "portfolio/W6/chat.png", "portfolio/W6/collage.png", "portfolio/W6/inspo.png", "portfolio/W6/image.png"]
 };
 let currentWorkshop = 1;
 let currentImageIndex = 0;
